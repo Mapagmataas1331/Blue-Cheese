ffbd8be0c4e4fe36c9921b584ab0fa67e9f52aa9
Mapagmataas <mapagmataas@outlook.com>
2023-10-18 12:14:15 +0700

    1331

--- app/src/main/java/com/example/list/view/SharedPreferencesManager.kt
+++ app/src/main/java/com/example/list/view/SharedPreferencesManager.kt
@@ -1,17 +1,37 @@
 package com.example.list.view
 
 import android.content.Context
-import android.content.SharedPreferences
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.encodeToJsonElement
 
 class SharedPreferencesManager(context: Context) {
-  private val sharedPreferences: SharedPreferences =
-    context.getSharedPreferences("MySharedPreferences", Context.MODE_PRIVATE)
-  fun saveItems(items: List<String>) {
-    sharedPreferences.edit()
-      .putStringSet("items", items.toSet())
+  private val sharedPreferences = context.getSharedPreferences(LIST_PREFS, Context.MODE_PRIVATE)
+
+  fun saveItems(list: List<ListItemViewModel>) {
+    val serializableSet = list.map {
+      Json.encodeToString(it)
+    }.toSet()
+    sharedPreferences
+      .edit()
+      .putStringSet(LIST_KEY, serializableSet)
       .apply()
   }
-  fun loadItems(): List<String> {
-    return sharedPreferences.getStringSet("items", emptySet())?.toList() ?: emptyList()
+    fun loadItems(): List<ListItemViewModel> = sharedPreferences.getStringSet(LIST_KEY, setOf())!!
+      .map {
+        Json.decodeFromString<ListItemViewModel>(it)
+      }.toList()
+
+//  fun saveItems(list: List<String>) {
+//    sharedPreferences.edit()
+//      .putStringSet(LIST_KEY, list.toSet())
+//      .apply()
+//  }
+//  fun loadItems(): List<String> {
+//    return sharedPreferences.getStringSet(LIST_KEY, emptySet())?.toList() ?: emptyList()
+//  }
+
+  companion object {
+    private const val LIST_PREFS = "LIST_PREFS"
+    private const val LIST_KEY = "LIST_KEY"
   }
 }
